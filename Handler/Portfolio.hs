7f5d6ccd5b9c322c0ef7681ceee3682e657043a1
Makoto Nishiura <nishiuramakoto@gmail.com>
2016-01-16 14:14:30 +0900

    experimenting css tree view

--- Handler/Portfolio.hs
+++ Handler/Portfolio.hs
@@ -2,9 +2,12 @@ module Handler.Portfolio where
 
 import Import
 
+
+categoryTree :: Widget
+categoryTree =  toWidget $(widgetFile "css-tree")
+
 getPortfolio01R :: Handler Html
 getPortfolio01R = defaultLayout $ do
   addStylesheet $ StaticR  css_normalize_css
   setTitle "水廻りの疑問"
-
   toWidget $(widgetFile "01")
