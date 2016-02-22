7fba5ae4894fc00eaee61e92abfd141e19821291
Makoto Nishiura <nishiuramakoto@gmail.com>
2016-02-22 23:46:31 +0900

    optimized prolog module

---
+++ app/Inquire.hs
@@ -0,0 +1,39 @@
+module Inquire (
+  PrologInquireBoolForm(..),
+  inquirePrologBool
+  )
+  where
+
+import Import
+import ContMap
+import Control.Monad.CC.CCCxe
+import Language.Prolog2.Syntax
+
+-------------------------- Primitive forms  --------------------------
+
+data PrologInquireBoolForm = PrologInquireBoolForm Bool
+                             deriving Show
+
+prologInquireBoolForm :: Term -> Html -> MForm Handler (FormResult PrologInquireBoolForm, Widget)
+prologInquireBoolForm t = renderDivs $ PrologInquireBoolForm
+                         <$> areq boolField (fromString $ show t) Nothing
+
+prologInquireBoolWidget ::  ContId -> Widget -> Enctype -> Widget
+prologInquireBoolWidget klabel formWidget _enctype = do
+  [whamlet| <form action=@{PrologExecuteTestContR klabel} method="GET">
+              ^{formWidget}
+              <button type="submit" value="Submit">Submit</button>
+  |]
+
+
+prologInquireBoolHtml :: Term -> CC (PS Html) Handler (ContId, Html)
+prologInquireBoolHtml t = do
+  (klabel, formWidget, enctype) <- lift $ generateCcFormGet $ prologInquireBoolForm t
+  html <- lift $ defaultLayout $ prologInquireBoolWidget klabel formWidget enctype
+  return (klabel, html)
+
+inquirePrologBool :: Term -> CC (PS Html) Handler (ContId, FormResult (PrologInquireBoolForm))
+inquirePrologBool t = do
+  (klabel, html) <- prologInquireBoolHtml  t
+  answer         <- inquireGet klabel html (prologInquireBoolForm t)
+  return (klabel, answer)
