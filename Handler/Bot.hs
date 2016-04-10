a2a71e2c4af5b42e61731849591a88f203ba8665
Makoto Nishiura <nishiuramakoto@gmail.com>
2016-04-10 12:37:37 +0900

    bot editor prototype

--- Handler/Bot.hs
+++ Handler/Bot.hs
@@ -4,12 +4,14 @@ module Handler.Bot (
   ) where
 
 import             Import hiding (parseQuery,readFile)
-import             CCGraph
 import             Control.Monad.Trans.Either
+import             Control.Monad.CC.CCCxe
+import             Data.Time.LocalTime
 import             DBFS
 import             Prolog
-import             Data.Time.LocalTime
+import             CCGraph
 import             Constructors
+import             Form hiding(directoryName,directoryExplanation)
 import             Show
 import qualified   Data.Text as T
 
@@ -31,4 +33,87 @@ getBotR = do
 
 getBotEditR :: DirectoryId -> Handler Html
 getBotEditR dir = do
-  redirect BotR
+  st <- startState
+  uid <- getUserAccountId
+  run $ editMain st uid dir
+
+editWidget :: CCState -> UserAccountId -> Directory -> CCNode -> Widget
+editWidget st uid dir node = do
+  setTitle "Bot editor"
+  addScript $ StaticR css_ace_src_noconflict_ace_js
+  addStylesheet $ StaticR css_bootstrap_css
+
+  let name = directoryName dir
+      expl = directoryExplanation dir
+      code = directoryCode dir
+  $(widgetFile "bot_editor")
+
+
+editHtml :: CCState -> UserAccountId -> Directory -> CCNode -> CC CCP Handler Html
+editHtml st uid dir node = do
+  lift $ defaultLayout $ editWidget st uid dir node
+
+inquireEdit :: CCState -> UserAccountId -> Directory -> CC CCP Handler CCState
+inquireEdit st uid dir = do
+  newNode <- inquire st (editHtml st uid dir)
+  return (newNode , formInj (FormSuccess ()))
+
+editFinishHtml :: CC CCP Handler Html
+editFinishHtml = lift $ redirect HomeR
+
+editMain :: CCState -> UserAccountId -> DirectoryId -> CC CCP Handler Html
+editMain st uid dir =  do
+   edata <- lift $ runDB $ readDirectory uid dir
+   case edata of
+     Right dirData -> inquireEdit st uid dirData >> return ()
+     Left  err     -> lift $ setMessage $ toHtml $ T.pack $ show err
+
+   editFinishHtml >>= inquireFinish
+
+-- loopEdit :: CCState -> UserAccountId -> DirectoryId -> Bool -> CC CCP Handler ()
+-- loopEdit st uid dir forceSave = do
+--   edata <- lift $ runDB $ readDirectory uid dir
+--   case edata of
+--     Right dirData -> loopEdit' dirData
+--     Left  err    -> do
+--       setMessage $ toHtml $ T.pack $ show err
+--       return ()
+
+--   where
+--     loopEdit' :: Directory -> CC CCP Handler ()
+--     loopEdit' dirData = do
+--       let uid' = directoryUserId dirData
+--           name = directoryName   dirData
+--           expl = directoryExplanation dirData
+--           code = directoryCode   dirData
+
+--       (st', mact) <- inquireDirectory st name expl code forceSave
+
+--       case (mact, getEditData st') of
+--         (Just Save, Just (newName,newExpl,newCode)) -> do
+--           write st' uid dirData { directoryName = newName
+--                                 , directoryExplanation = newExpl
+--                                 , directoryCode = newCode
+--                                 }
+--             forceSave
+--         _ -> loopEdit st' uid dir forceSave
+
+--     write st uid dirData forceSave = do
+--       if forceSave || programOK newCode
+--         then do runDB $ writeDirectory uid dirData
+--                 loopEdit st uid dirData forceSave
+--           else
+--   ((node, form), mact) <- inquireDirectory st  "" (Textarea "") (Textarea "") forceSave
+--   let mdata = getEditData form
+--   case (mact, mdata) of
+--     (Just Save, Just (expl,code)) -> save st uid dir (expl,code) forceSave
+--     _ loopEdit st uid dir forceSave
+
+
+
+getEditData (FormDirectoryForm
+          (FormSuccess (DirectoryForm name
+                        (Textarea expl)
+                        (Textarea code))))
+  = Just (name,expl,code)
+getEditData _ = Nothing
