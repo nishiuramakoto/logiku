3b5bf5a9e40aa95d2f132402bc85d4273b6306f3
Makoto Nishiura <nishiuramakoto@gmail.com>
2016-04-16 15:26:00 +0900

    simplified

--- scripts/rouki-2.pl
+++ scripts/rouki-2.pl
@@ -1,50 +1,48 @@
 :- use_module(library(lists)).
 
 法令違反 :- 法令違反(_).
-法令違反(X) :- 不法(X) ; 有期労働契約基準違反(X).
+法令違反(Case) :- 不法(Case) ; 有期労働契約基準違反(Case).
 不法 :- 不法(_).
 
 
-無効労働契約部分(X) :- 確認要件(労働契約(Y)),
-                       確認要件(契約の一部(X,Y)),
-                       確認要件(労基法の基準に逹しない(Y)).
+不法(Case) :- 事件(Case, 劣悪労働条件) ,
+	      要件(Case,労働条件が労基法の基準に逹しない(Case)).
 
 
-不法(_) :- 事件(Case,労働契約期間の違反),
-           確認要件(Case,契約期間年数 > 3),
-           不該当要件(Case,労働契約期間の例外(Case)).
-
+不法(Case) :- 事件(Case,労働契約期間の違反),
+              要件(Case,契約期間年数(Case) > 3),
+              要件(Case, \+ 労働契約期間の例外(Case)).
 
 % 法附則137条
-不法(_) :- 事件(Case,早期任意退職),
-	   ( 要件(Case, 労働契約期間の例外(Case)),
-	     確認要件(Case, 契約期間内に任意退職)
-	     ;
-	     不該当要件(Case, 労働契約期間の例外(Case)),
-	     ( 確認要件(Case, (労働契約期間年数(Case,D) , D =< 1)) ;
-	       確認要件(Case, (退職日が1年経過せず(Case) )))).
+不法(Case) :- 事件(Case,早期任意退職),
+	      ( 要件(Case, 労働契約期間の例外(Case)),
+		要件(Case, 契約期間内に任意退職)
+		;
+		要件(Case, \+ 労働契約期間の例外(Case)),
+		( 要件(Case, (労働契約期間年数(Case,D) , D =< 1)) ;
+		  要件(Case, (退職日が1年経過せず(Case) )))).
 
 
-労働契約期間の例外(Case) :- 確認要件(Case,一定の事業の完了に必要な期間を定める労働契約) ,
-                            確認要件(Case, 契約期間終了日 =< 終期)
+労働契約期間の例外(Case) :- 要件(Case,一定の事業の完了に必要な期間を定める労働契約) ,
+                            要件(Case, 契約期間終了日 =< 終期)
                             ;
-                            確認要件(Case, 認定職業訓練を受ける労働者に係る労働契約) ,
-                            確認要件(Case, 契約期間終了日 =< 終期)
+                            要件(Case, 認定職業訓練を受ける労働者に係る労働契約) ,
+                            要件(Case, 契約期間終了日 =< 終期)
                             ;
                             ( 要件(Case, 専門知識労働者等の労働契約(Case)) ;
-                              確認要件(Case, 満60歳以上労働者の労働契約 )),
-                            確認要件(Case, 契約期間年数 =< 5).
+                              要件(Case, 満60歳以上労働者の労働契約 )),
+                            要件(Case, 契約期間年数 =< 5).
 
-専門知識労働者等の労働契約(Case) :- 確認要件(Case,労働契約(Case,_Employer,Employee)),
+専門知識労働者等の労働契約(Case) :- 要件(Case,労働契約(Case,_Employer,Employee)),
                                     要件(Case,専門知識労働者(Case,Employee,K)),
-				    確認要件(Case,従事(Case,Employee,K)).
+				    要件(Case,従事(Case,Employee,K)).
 
 
-専門知識労働者(Case,E,K) :- 専門知識リスト(L), 確認要件リスト(Case,有する専門知識等,L,K)
+専門知識労働者(Case,E,K) :- 専門知識リスト(L), 要件リスト(Case,有する専門知識等,L,K)
 			    ;
-			    member(K,[ 農林水産業, システムエンジニア ]),
-			    確認要件(Case, (年収万円(E,Salary), Salary >= 1075)) ,
-			    確認要件(Case, 保持(E,K)).
+			    要件リスト(Case,有する専門知識等,[ 農林水産業, システムエンジニア ],K),
+			    要件(Case, (年収万円(E,Salary), Salary >= 1075)) ,
+			    要件(Case, 保持(E,K)).
 
 
 専門知識リスト([ 博士の学位,
@@ -56,90 +54,75 @@
 
 有期労働契約基準違反 :- 有期労働契約基準違反(_Case).
 
-有期労働契約基準違反(_) :- 事件(Case, 雇止めの予告),
-			   有期労働契約基準対象契約1(Case),
-			   (確認要件(Case,(雇止めの予告日(Case,Day1), 契約期間満了日(Case,Day2), Day2 - Day1 < 30))
-			    ;
-			    確認要件(Case,(使用者が雇止め前又は後の不更新理由請求に対し遅滞なく証明書を交付しない))).
+有期労働契約基準違反(Case) :- 事件(Case, 雇止めの予告),
+			      有期労働契約基準対象契約1(Case),
+			      (要件(Case,(雇止めの予告日(Case,Day1), 契約期間満了日(Case,Day2), Day2 - Day1 < 30))
+			       ;
+			       要件(Case,(使用者が雇止め前又は後の不更新理由請求に対し遅滞なく証明書を交付しない))).
 
-有期労働契約基準違反(_) :- 事件(Case, 契約期間の配慮),
-			   有期労働契約基準対象契約2(Case),
-			   (確認要件(Case, 使用者が有期契約を更新しようとした),
-			    確認不該当要件(Case, 契約の実態及び労働者の希望に応じできるだけ契約期間を長くするよう努めた)).
+有期労働契約基準違反(Case) :- 事件(Case, 契約期間の配慮),
+			      有期労働契約基準対象契約2(Case),
+			      (要件(Case, 使用者が有期契約を更新しようとした),
+			       要件(Case, \+ 使用者が契約の実態及び労働者の希望に応じできるだけ契約期間を長くするよう努めた)).
 
-有期労働契約基準対象契約1(Case) :- 確認要件(Case, 有期労働契約(Case)) ,
-				   ( 確認要件(Case, (更新回数(Case,N) , N >= 3))
-				     ; 確認要件(Case, 継続勤務1年超)),
-				   確認不該当要件(Case, あらかじめ不更新を明示).
+有期労働契約基準対象契約1(Case) :- 要件(Case, 有期労働契約(Case)) ,
+				   ( 要件(Case, (更新回数(Case,N) , N >= 3))
+				     ; 要件(Case, 継続勤務1年超)),
+				   要件(Case, \+ あらかじめ不更新を明示).
 
-有期労働契約基準対象契約2(Case) :- 確認要件(Case, 有期労働契約(Case)) ,
-				   ( 確認要件(Case, (更新回数(Case,N) , N >= 1))
-				     , 確認要件(Case, 継続勤務1年超)).
+有期労働契約基準対象契約2(Case) :- 要件(Case, 有期労働契約(Case)) ,
+				   ( 要件(Case, (更新回数(Case,N) , N >= 1))
+				     , 要件(Case, 継続勤務1年超)).
 
 
 %% Implementation
 
-事件(Case, Desc) :- 該当(Case, 事件(Desc)),
-                    assertz(caseDescription(Desc)),
-                    write(事件),put_char(':'), write(Desc), nl.
+:- dynamic 事件書き出し/0.
+:- dynamic 事件記述/1.
 
-要件(Case,Cond) :- 該当(Case,要件(Cond)).
-確認要件(Case,Cond) :- 該当(Case,確認要件(Cond)).
-確認要件リスト(Case,Desc,List,Elem) :- write(choose_number),nl,
-				       write(Case), write(Desc),nl,
-				       write(List),
-				       read(N),
-				       nth0(N,List,Elem).
+事件書き出し :- fail.
+
+事件(Case, Desc) :- 事件書き出し ,
+                    assertz(事件記述(Desc)),
+                    write(事件),put_char(':'), write(Desc), nl , !
+		    ;
+		    Case = Desc.
+
+要件(Case,Cond) :-  事件書き出し , !
+		    ;
+		    write(要件), print(Cond),
+		    catch(Cond, Err, 確認要件(Err,Case,Cond)).
+
+確認要件(Err,Case,Cond) :- write(yes_no_or_dontKnow),nl,
+			   write(Err),nl,
+			   write(Cond),nl,
+			   read(Answer),
+			   確認要件(Err,Case,Cond,Answer).
+
+確認要件(_Err,_Case,_Cond, yes).
+確認要件(_Err, Case, Cond, dontKnow) :- write(確認要件), 要件(Case, Cond).
+
+要件リスト(Case,Desc,List,Elem) :- write(choose_number),nl,
+				   write(Case), write(Desc),nl,
+				   write(List),
+				   read(N),
+				   nth0(N,List,Elem).
 
 
-確認不該当要件(Case,Cond) :- 該当(Case,確認不該当要件(Cond)).
-不該当要件(Case,Cond) :- 該当(Case,不該当要件(Cond)).
+
+事件リスト(Ds) :- retractall(事件記述(Desc)),
+		  retractall(事件書き出し),
+                  asserta(事件書き出し),
+                  bagof(X,法令違反(X),_),
+                  bagof(Desc, 事件記述(Desc),Ds),
+                  retractall(事件記述(Desc)),
+		  retractall(事件書き出し),
+		  asserta(事件書き出し :- fail).
 
 
-
-
-descList(Ds) :- retractall(該当(Case,Desc)),
-                retractall(不該当(Case,Desc)),
-                retractall(caseDescription(Desc)),
-                retractall(condition(Case,Desc)),
-                asserta(該当(Case,Desc)),
-                asserta(不該当(Case,Desc)),
-                bagof(X,法令違反(X),_),
-                bagof(Desc, caseDescription(Desc),Ds),
-                retractall(該当(Case,Desc)),
-                retractall(不該当(Case,Desc)),
-                retractall(caseDescription(Desc)),
-                retractall(condition(Case,Desc)).
-
-
-事件該当(_Case,Desc, Desc).
-
-要件該当(_Case,Cond) :-  call(Cond).
-
-確認要件該当(Case,Cond) :- write(Case),
-                       write(Cond),
-                       read(Answer),
-                       Answer = yes.
-
-要件不該当(_Case,Cond) :- \+ call(Cond).
-
-確認要件不該当(Case,Cond) :- write(Case),
-                             write(Cond),
-                             read(Answer),
-                             Answer = no.
-
-:- dynamic 該当/2.
-
-inquire :- descList(DescList),
-           write(choose_number),nl,
-           write(DescList) ,
-           read(N),
-           nth0(N,DescList,MyDesc),
-
-           asserta(該当(Case,事件(Desc)) :- 事件該当(Case,Desc,MyDesc) ),
-           asserta(該当(Case,要件(Desc)) :- 要件該当(Case,Desc) ),
-           asserta(該当(Case,確認要件(Desc)) :- 確認要件該当(Case,Desc) ),
-           asserta(該当(Case,不該当要件(Desc)) :- 要件不該当(Case,Desc)),
-           asserta(該当(Case,確認不該当要件(Desc)) :- 確認要件不該当(Case,Desc)),
-           法令違反,
-           retractall(該当(Case,Desc)).
+chooseCase :- 事件リスト(List),
+              write(choose_number),nl,
+              write(List) ,
+              read(N),
+              nth0(N,List,Desc),
+              法令違反(Desc).
--- scripts/test.pl
+++ scripts/test.pl
@@ -3,7 +3,16 @@ a :- f(_).
 f(0) :- b(0).
 f(1) :- b(1).
 
-b(X) :- write_term(X,[]),
-    assertz(d(X)).
 
-g(L) :- f(0),f(1), bagof(X,d(X),L).
+要件(X) :- catch(X, Err, 確認要件(X,Err)).
+
+確認要件(X,Err) :- write(Err),nl,
+		   write(X),nl,
+		   read(Answer),
+		   確認要件(X,Err,Answer).
+
+確認要件(_X,_Err,yes).
+確認要件(X,_Err,dontKnow) :- 要件(X).
+
+b :- fail.
+inquire :- b, 要件(b).
